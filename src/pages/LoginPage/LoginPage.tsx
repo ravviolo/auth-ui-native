20026d372c2a0676a5fdd837232d4d8118e46fe3
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-16 14:42:18 +0200

    Define click handlers in pages

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 interface Props {
@@ -7,6 +8,32 @@ interface Props {
 export const LoginPage = ({ navigate }: Props) => {
   const handleNavigate = () => {
     navigate('signup');
+  };
+
+  const handleLoginFormSubmit = (email: string, password: string): void => {
+    const userCredentials = {
+      email,
+      password,
+    };
+
+    console.log('Logging user...');
+    console.log(userCredentials);
+  };
+
+  const handleLoginViaGoogle = (): void => {
+    console.log('Logging user via Google');
+  };
+
+  const handleLoginViaLinkedIn = (): void => {
+    console.log('Logging user via LinkedIn');
+  };
+
+  const handleLoginViaFacebook = (): void => {
+    console.log('Logging user via Facebook');
+  };
+
+  const handleResetPassword = (): void => {
+    console.log('Reset password');
   };
 
   return (
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 interface Props {
@@ -7,6 +8,28 @@ interface Props {
 export const SignUpPage = ({ navigate }: Props) => {
   const handleNavigate = () => {
     navigate('login');
+  };
+
+  const handleSignUpFormSubmit = (email: string, password: string): void => {
+    const userCredentials = {
+      email,
+      password,
+    };
+
+    console.log('Registering user...');
+    console.log(userCredentials);
+  };
+
+  const handleSignUpViaGoogle = (): void => {
+    console.log('Registering user via Google');
+  };
+
+  const handleSignUpViaLinkedIn = (): void => {
+    console.log('Registering user via LinkedIn');
+  };
+
+  const handleSignUpViaFacebook = (): void => {
+    console.log('Registering user via Facebook');
   };
 
   return (
