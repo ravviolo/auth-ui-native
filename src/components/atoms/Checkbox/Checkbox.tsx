61be545c74fca5dd03051984ca9fac2bfc2e225f
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-19 20:46:17 +0200

    Add Checkbox

---
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,41 @@
+import ExpoCheckbox from 'expo-checkbox';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { colors } from 'theme';
+
+export interface CheckboxHandle {
+  getValue: () => boolean;
+  clear: () => void;
+}
+
+interface Props {
+  testID: string;
+}
+
+export const Checkbox = forwardRef<CheckboxHandle, Props>(({ testID }, ref) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => isChecked,
+    clear: () => {
+      setIsChecked(false);
+    },
+  }));
+
+  return (
+    <ExpoCheckbox
+      color={isChecked ? colors.primary : colors.textLight}
+      style={styles.checkbox}
+      testID={testID}
+      value={isChecked}
+      onValueChange={setIsChecked}
+    />
+  );
+});
+
+const styles = StyleSheet.create({
+  checkbox: {
+    borderRadius: 4,
+    borderWidth: 1,
+  },
+});
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,6 @@
 export { Button } from './Button/Button';
+export { Checkbox } from './Checkbox/Checkbox';
+export type { CheckboxHandle } from './Checkbox/Checkbox';
 export { Flex } from './Flex/Flex';
 export { Heading } from './Heading/Heading';
 export { InputField } from './InputField/InputField';
