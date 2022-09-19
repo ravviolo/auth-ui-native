c0f3bc0b947a4ad1968e736e0ecdcaf55a8baf3d
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-19 12:31:19 +0200

    Add Button

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { colors } from 'theme';
+
+interface Props {
+  title: string;
+  testID: string;
+  onPress: () => void;
+}
+
+export const Button = ({ onPress, testID, title }: Props) => {
+  return (
+    <TouchableHighlight
+      style={styles.btnContainer}
+      testID={testID}
+      underlayColor={colors.primaryDark}
+      onPress={onPress}
+    >
+      <Text style={styles.btnText}>{title}</Text>
+    </TouchableHighlight>
+  );
+};
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    alignItems: 'center',
+    backgroundColor: colors.primary,
+    borderRadius: 10,
+    elevation: 5,
+    padding: 15,
+  },
+  btnText: {
+    color: colors.textWhite,
+    letterSpacing: 1,
+    textTransform: 'uppercase',
+  },
+});
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
-export { Heading } from './Heading/Heading';
+export { Button } from './Button/Button';
 export { Flex } from './Flex/Flex';
+export { Heading } from './Heading/Heading';
 export { TextButton } from './TextButton/TextButton';
