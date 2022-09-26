ef6ff00b71ae9d079d6fbd442a3646c18ef699d7
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-26 13:35:24 +0200

    Add Checkbox stories

---
+++ src/components/atoms/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,9 @@
+import { text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react-native';
+import { StoryScreen } from 'storybook/StoryScreen';
+
+import { Checkbox } from './Checkbox';
+
+storiesOf('Checkbox', module)
+  .addDecorator((getStory) => <StoryScreen>{getStory()}</StoryScreen>)
+  .add('Default', () => <Checkbox testID={text('testID', 'test-id')} />);
--- src/storybook/stories/index.ts
+++ src/storybook/stories/index.ts
@@ -1,1 +1,2 @@
 import 'components/atoms/Button/Button.stories';
+import 'components/atoms/Checkbox/Checkbox.stories';
