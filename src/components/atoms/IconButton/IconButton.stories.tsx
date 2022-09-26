27668ec221c7ab059e45593c1e6c98be46681f7a
Jolanta Ślipko <j.slipko21@gmail.com>
2022-09-26 14:05:21 +0200

    Add IconButton stories

---
+++ src/components/atoms/IconButton/IconButton.stories.tsx
@@ -0,0 +1,19 @@
+import { action } from '@storybook/addon-actions';
+import { color, text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react-native';
+import { StoryScreen } from 'storybook/StoryScreen';
+
+import { IconButton } from './IconButton';
+
+storiesOf('IconButton', module)
+  .addDecorator((getStory) => (
+    <StoryScreen style={{ alignItems: 'center' }}>{getStory()}</StoryScreen>
+  ))
+  .add('Default', () => (
+    <IconButton
+      color={color('color', '#4267B2')}
+      icon={text('icon', 'f')}
+      testID={text('testID', 'icon-btn-test-id')}
+      onPress={action('onPress')}
+    />
+  ));
--- src/storybook/stories/index.ts
+++ src/storybook/stories/index.ts
@@ -3,3 +3,4 @@ import 'components/atoms/Checkbox/Checkbox.stories';
 import 'components/atoms/Divider/Divider.stories';
 import 'components/atoms/Flex/Flex.stories';
 import 'components/atoms/Heading/Heading.stories';
+import 'components/atoms/IconButton/IconButton.stories';
